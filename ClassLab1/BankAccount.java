9c17578fbefb3f604a3677cf3e00f8ac370232dd
nikhilv <nikhil.v@mscsa.christuniversity.in>
2023-08-12 12:10:22 +0530

    Add files via upload

--- ClassLab1/BankAccount.java
+++ ClassLab1/BankAccount.java
@@ -1,50 +1,9 @@
 package ClassLab1;
 
-public class BankAccount {
-    String accountNumber;
-    String accountHolderName;
-    double balance;
-    String accountType;
+public abstract class BankAccount {
+  protected   String accountNumber;
+   protected String accountHolderName;
+   protected double balance;
+   protected String accountType;
 
-    public BankAccount() {
-    }
-
-    public BankAccount(String accountNumber, String accountHolderName, double balance, String accountType) {
-        this.accountNumber = accountNumber;
-        this.accountHolderName = accountHolderName;
-        this.balance = balance;
-        this.accountType = accountType;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public String getAccountHolderName() {
-        return accountHolderName;
-    }
-
-    public void setAccountHolderName(String accountHolderName) {
-        this.accountHolderName = accountHolderName;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
-    public String getAccountType() {
-        return accountType;
-    }
-
-    public void setAccountType(String accountType) {
-        this.accountType = accountType;
-    }
 }
